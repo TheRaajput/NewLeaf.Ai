ee326584c06cfef3cc894568c2cf49895a743bd2
TheRaajput <yasharth.cse@gmail.com>
2021-06-09 15:53:32 +0530

    init

--- src/Component/Result/ResContainer.js
+++ src/Component/Result/ResContainer.js
@@ -5,6 +5,7 @@ const ResContainer = (props) => {
   return (
     <Col className="boxes ml-2">
       <h4 className="text-center">{props.title}</h4>
+      <p className="p-1">{props.details}</p>
     </Col>
   );
 };
